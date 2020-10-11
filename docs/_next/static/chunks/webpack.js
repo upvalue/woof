da181baeb3fafb8d5e022101ff7173a6bdb9814f
Phil <phil@upvalue.io>
2020-10-11 17:36:37 -0500

    once more

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "400bda7886a438b17851";
+/******/ 	var hotCurrentHash = "747f5790513a51fa42e8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
